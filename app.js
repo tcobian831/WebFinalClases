a8de9abd3a7954f3c0f76f60cd6e82d51c9406e4
tcobian831 <tcobian@itba.edu.ar>
2025-06-23 11:29:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -300,6 +300,7 @@ var ui = {
                 html += '<td>' + alumno.nombre + '</td>';
                 html += '<td>' + alumno.email + '</td>';
                 html += '<td>' + alumno.telefono + '</td>';
+                html += '<td>' + (alumno.nivel || 'No especificado') + '</td>';
                 html += '<td class="action-buttons">';
                 html += '<button class="btn btn-sm btn-secondary btn-editar-alumno" data-id="' + alumno.id + '">Editar</button>';
                 html += '<button class="btn btn-sm btn-danger btn-eliminar-alumno" data-id="' + alumno.id + '">Eliminar</button>';
